6c7f4a2c81cb2b0f3f9162974b69433fa121704c
Sébastien Canonica <sebastien.canonica@360learning.com>
2021-07-29 10:56:44 +0200

    conf

---
+++ TypeScript/wallaby.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+    return {
+        files: [
+            "src/**/*.ts",
+            "tests/**/*.txt"
+        ],
+        tests: [
+            "tests/**/*.ts"
+        ],
+        env: {
+            type: "node",
+            runner: "node"
+        },
+    };
+};
