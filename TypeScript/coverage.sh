51fb9962e5af13afbbf2f82bff96e93a6e53d27f
Thierry Lalande <thierry.lalande@360learning.com>
2023-09-13 15:17:23 +0200

    fix coverage.sh

--- TypeScript/coverage.sh
+++ TypeScript/coverage.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./node_modules/.bin/ncy ./node_modules/mocha/bin/mocha --recursive --require ts-node/register "tests/**/*.test.ts"
+./node_modules/.bin/nyc ./node_modules/mocha/bin/mocha --recursive --require ts-node/register "tests/**/*.test.ts"
